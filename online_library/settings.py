a08565f25daed071ffc7a02bfa823c3cbdfd9f4b
ChristophorosFeggaris <cfeggaris@gmail.com>
2022-08-14 01:34:11 +0300

    change static_root

--- online_library/settings.py
+++ online_library/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 """STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]"""
